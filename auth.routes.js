9de498c450c23e399e48a2970cbcf6255f1e4910
Merovinci <denis123dexter8@gmail.com>
2026-09-02 20:50:20 -0300

    Delete auth.routes.js

--- auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { login } = require("../controllers/auth.controller");
-const { loginLimiter } = require("../middleware/rateLimiter");
-
-const router = express.Router();
-router.post("/login", loginLimiter, login);
-
-module.exports = router;
